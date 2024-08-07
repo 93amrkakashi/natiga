ae6984edf530bc3555e1a1791d83003ca3e46ff7
93amrkakashi <93amr.kakashi@gmail.com>
2024-08-06 19:33:18 -0700

    البالباي

--- app/api/proxyNatiga/route.js
+++ app/api/proxyNatiga/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import * as cheerio from 'cheerio';
+
 export async function POST(request) {
   try {
     const { seating_no } = await request.json();
@@ -11,7 +12,7 @@ export async function POST(request) {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Cookie': '_ga=GA1.1.2025489188.1722950036; __gads=ID=9489f15c06ac099c:T=1722950209:RT=1722950209:S=ALNI_MaEiZD4NVFSKDUWy0lJsKCesxC9jA; __gpi=UID=00000ebbc05cedd1:T=1722950209:RT=1722950209:S=ALNI_MYwFqEbThAq3H7WRT1A_tzKUlZQug; __eoi=ID=fc36fdbaa4bd9d3c:T=1722950209:RT=1722950209:S=AA-AfjbIT7eCc2fQfxdnvtugzp9V; _ga_J7S2SZJ3N7=GS1.1.1722950036.1.1.1722950210.0.0.0; _ga_56XSY8ZTS3=GS1.1.1722950035.1.1.1722950210.0.0.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'en-US',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.127 Safari/537.36',
@@ -30,11 +31,17 @@ export async function POST(request) {
     }
 
     const data = await response.text();
-    // استخدام cheerio لتحليل HTML
     const $ = cheerio.load(data);
-    const resultInfo = $('.result-info').html(); // استخراج المحتوى داخل div بالتصنيف result-info
 
-    return new NextResponse(resultInfo, { headers: { 'Content-Type': 'text/html' } });
+    const resultInfo = $('.result-info').html(); 
+    const studentInfo = $('.full-result').html(); 
+
+    const combinedContent = `
+    <div class="halfinput-student">${studentInfo}</div>
+      <div class="result-info">${resultInfo}</div>
+    `;
+
+    return new NextResponse(combinedContent, { headers: { 'Content-Type': 'text/html' } });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
--- app/page.js
+++ app/page.js
@@ -1,43 +1,73 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const Natiga = () => {
+  const [seatingNo, setSeatingNo] = useState('');
   const [data, setData] = useState('');
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/proxyNatiga', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ seating_no: '856541' }),
-        });
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('/api/proxyNatiga', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ seating_no: seatingNo }),
+      });
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const textData = await response.text();
-        setData(textData);
-      } catch (error) {
-        setError(error.message);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
-    };
 
+      const textData = await response.text();
+      setData(textData);
+      setError('');
+    } catch (error) {
+      setError(error.message);
+      setData('');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
     fetchData();
-  }, []);
+  };
 
   return (
-    <div>
-      <h1>نتيجة البحث</h1>
+    <div className="min-h-screen flex flex-col items-center justify-start gap-4 bg-gray-100 p-4 pt-8">
+      <h1 className="text-3xl font-bold my-8">
+       الآن نتيجة الثانوية العامة 2024 مع درجات المواد بالتفصيل
+        </h1>
+      <h1 className="text-xl font-bold mb-4">ادخل رقم جلوسك</h1>
+      <form onSubmit={handleSubmit} className="w-[60%] p-6 rounded-lg shadow-lg flex flex-col gap-4">
+        <input
+          type="text"
+          value={seatingNo}
+          onChange={(e) => setSeatingNo(e.target.value)}
+          placeholder="رقم الجلوس"
+          className="w-full p-2 border border-gray-300 rounded-lg"
+        />
+        <button 
+        disabled={!seatingNo}
+          type="submit" 
+          className="w-full bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition"
+        >
+          عرض النتيجة
+        </button>
+      </form>
+
+      {loading && <p className="mt-4 text-blue-500">جاري تحميل البيانات...</p>}
+
       {error ? (
-        <p>حدث خطأ: {error}</p>
+        <p className="mt-4 text-red-500">حدث خطأ: {error}</p>
       ) : (
-        <div dangerouslySetInnerHTML={{ __html: data }} />
+        <div className="mt-4" dangerouslySetInnerHTML={{ __html: data }} />
       )}
     </div>
   );
